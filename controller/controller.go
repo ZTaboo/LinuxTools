7a6696b38dd0c0a11309ba3805f0ba0b3e00a808
zero <166997982@qq.com>
2021-10-22 21:04:55 +0800

    新增更换系统源

--- controller/controller.go
+++ controller/controller.go
@@ -1,8 +1,13 @@
 package controller
 
 import (
+	"bufio"
+	_ "embed"
 	"fmt"
+	"io"
+	"os"
 	"os/exec"
+	"sync"
 )
 
 func Version(data string) {
@@ -13,13 +18,104 @@ func Version(data string) {
 	}
 }
 
+//go:embed replace/ubuntu16.04.txt
+var ubuntu16 string
+
+//go:embed replace/ubuntu18.04.txt
+var ubuntu18 string
+
+//go:embed replace/ubuntu20.04.txt
+var ubuntu20 string
+
 func ReplaceOsSource(data string) {
 	if data == "true" {
 		cmd := exec.Command("lsb_release", "-r")
 		con, _ := cmd.Output()
 		switch string(con[9 : len(con)-1]) {
 		case "20.04":
-			fmt.Println("this is 20.04")
+			file, _ := os.OpenFile("/etc/apt/sources.list", os.O_WRONLY|os.O_TRUNC, 0777)
+			defer file.Close()
+			write, err := file.Write([]byte(ubuntu20))
+			if err != nil {
+				fmt.Println("写入错误：", err)
+			} else {
+				cmd1 := exec.Command("sudo", "apt", "update", "-y")
+				con1, _ := cmd1.StdoutPipe()
+				var wg sync.WaitGroup
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					reader := bufio.NewReader(con1)
+					for {
+						resString, err := reader.ReadString('\n')
+						if err == io.EOF {
+							return
+						}
+						fmt.Printf(resString)
+					}
+				}()
+				err = cmd1.Start()
+				wg.Wait()
+				fmt.Println(con1)
+				fmt.Printf("写入成功，本地成功写入%d字节\n", write)
+			}
+		case "18.04":
+			file, _ := os.OpenFile("/etc/apt/sources.list", os.O_WRONLY|os.O_TRUNC, 0777)
+			defer file.Close()
+			write, err := file.Write([]byte(ubuntu18))
+			if err != nil {
+				fmt.Println("写入错误：", err)
+			} else {
+				cmd1 := exec.Command("sudo", "apt", "update", "-y")
+				con1, _ := cmd1.StdoutPipe()
+				var wg sync.WaitGroup
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					reader := bufio.NewReader(con1)
+					for {
+						resString, err := reader.ReadString('\n')
+						if err == io.EOF {
+							return
+						}
+						fmt.Printf(resString)
+					}
+				}()
+				err = cmd1.Start()
+				wg.Wait()
+				fmt.Println(con1)
+				fmt.Printf("写入成功，本地成功写入%d字节\n", write)
+			}
+		case "16.04":
+			file, _ := os.OpenFile("/etc/apt/sources.list", os.O_WRONLY|os.O_TRUNC, 0777)
+			defer file.Close()
+			write, err := file.Write([]byte(ubuntu16))
+			if err != nil {
+				fmt.Println("写入错误：", err)
+			} else {
+				cmd1 := exec.Command("sudo", "apt", "update", "-y")
+				con1, _ := cmd1.StdoutPipe()
+				var wg sync.WaitGroup
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					reader := bufio.NewReader(con1)
+					for {
+						resString, err := reader.ReadString('\n')
+						if err == io.EOF {
+							return
+						}
+						fmt.Printf(resString)
+					}
+				}()
+				err = cmd1.Start()
+				wg.Wait()
+				fmt.Println(con1)
+				fmt.Printf("写入成功，本地成功写入%d字节。\n", write)
+			}
+		default:
+			fmt.Println("本工具目前仅支持：ubuntu20，18,16版本系统")
+			fmt.Println("您的系统版本为：", con)
 		}
 	}
 }
